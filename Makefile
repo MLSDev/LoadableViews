ce656445334d4813376186d7ba445959d59a3305
Denys Telezhkin <strangervir@gmail.com>
2016-03-05 13:48:29 +0200

    add CI builds that run tests on iOS, tvOS, report test coverage, and verify carthage build.

---
+++ Makefile
@@ -0,0 +1,44 @@
+SHELL := /bin/bash
+# Install Tasks
+
+install-iOS:
+	true
+
+install-tvOS:
+	true
+
+install-carthage:
+	brew rm carthage || true
+	brew install carthage
+
+install-coverage:
+	true
+
+# Run Tasks
+
+test-iOS:
+	set -o pipefail && \
+		xcodebuild \
+		-project LoadableView.xcodeproj \
+		-scheme LoadableView \
+		-destination "name=iPhone 6s" \
+		clean test \
+		| xcpretty -ct
+
+test-tvOS:
+	set -o pipefail && \
+		xcodebuild \
+		-project LoadableView.xcodeproj \
+		-scheme LoadableView \
+		-destination "name=Apple TV 1080p" \
+		clean test \
+		| xcpretty -ct
+
+test-carthage:
+	carthage build --no-skip-current
+	ls Carthage/build/iOS/LoadableView.framework
+	ls Carthage/build/tvOS/LoadableView.framework
+
+test-coverage:
+	  set -o pipefail && xcodebuild -project LoadableView.xcodeproj -scheme LoadableView -enableCodeCoverage YES test | xcpretty -ct
+		bash <(curl -s https://codecov.io/bash)
--- Source/LoadableView.swift
+++ Source/LoadableView.swift
@@ -3,8 +3,25 @@
 //  LoadableView
 //
 //  Created by Denys Telezhkin on 05.03.16.
-//  Copyright © 2016 Denys Telezhkin. All rights reserved.
+//  Copyright © 2016 MLSDev Inc(https://mlsdev.com).
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import Foundation
 import UIKit
@@ -20,26 +37,26 @@ extension NibDefinable {
 }
 
 @IBDesignable public class LoadableView: UIView, NibDefinable {
-    
+
     @IBOutlet public weak var view : UIView!
-    
+
     public override init(frame: CGRect) {
         super.init(frame: frame)
         xibSetup()
     }
-    
+
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         xibSetup()
     }
-    
+
     public func xibSetup() {
         view = loadViewFromXib()
         view.frame = bounds
         view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         addSubview(view)
     }
-    
+
     func loadViewFromXib() -> UIView {
         let bundle = NSBundle(forClass: self.dynamicType)
         let nib = UINib(nibName: nibName, bundle: bundle)
